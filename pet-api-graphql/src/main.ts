c6293e80113bfd9013a6984735a31fb6070e33d1
obomheire <obomheire@gmail.com>
2023-04-02 17:59:37 +0100

    Updatded

--- pet-api-graphql/src/main.ts
+++ pet-api-graphql/src/main.ts
@@ -1,12 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { GlobalExceptionsFilter } from './utils/globalExceptionsFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-   app.useGlobalFilters(new GlobalExceptionsFilter());
+  app.useGlobalFilters();
   await app.listen(5000);
 }
 bootstrap();
